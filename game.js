03e778d48b53f36410e81bd02af52c5436fe091c
Anamika Pandey <83291071+anamika62@users.noreply.github.com>
2022-08-04 18:39:13 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,117 @@
+console.log('Game');
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressbarfull')
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswres = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch("questions.json")
+    .then(res => {
+        return res.json();
+    }).then(loadedQuestions => {
+        console.log(loadedQuestions);
+        // loadedQuestions.results.map(loadedQuestions =>{
+        //   const formattedQuestion ={
+        //       question:loadedQuestions.question
+        //   };
+        //   const answerChoices  =[...loadedQuestions.incorrect_answers];
+        //   formattedQuestion.answer =Math.floor(Math.random()*5)+1;
+        //   answerChoices.splice(formattedQuestion.answer -1,0,
+        //   loadedQuestions.correct_answer);
+
+        //   answerChoices.forEach((choice,index)=>{
+        //       formattedQuestion["choice" + (index+1)] = choice;
+        //   })
+        //   return formattedQuestion;
+        // });
+        questions = loadedQuestions;
+        startGame();
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 5;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    console.log(availableQuestions);
+    getNewQuestion();
+
+};
+
+getNewQuestion = () => {
+
+    if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
+
+        return window.location.assign("end.html");
+
+    }
+    questionCounter++;
+    progressText.innerText = `Question ${questionCounter} / ${MAX_QUESTIONS}`;
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswres = true;
+
+};
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        console.log(e.target);
+
+        if (!acceptingAnswres) return;
+
+        acceptingAnswres = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        if (classToApply == 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 300);
+
+    });
+});
+incrementScore = num => {
+    score += num;
+    scoreText.innerText = score;
+}
+
+
+
+
+
+
+
+
+
+
+
